e07ba83ac46e47939051d5dce64693d78cc917ff
zafercinpolat <cinpolatzafer@gmail.com>
2023-05-09 23:47:01 +0100

    first commit

---
+++ Plug.Reflection/SDK/Helper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDK
+{
+    public class Plug
+    {
+        public string pName;
+        public string pFName;
+        public string pPath;
+    }
+
+    public interface IPlugin
+    {
+        string action(string text);
+        string name();
+    }
+    public class Kit
+    {
+        public static List<Plug> GetAllPlugins(string path)
+        {
+            List<Plug> myPlugs = new List<Plug>();
+            if (!Directory.Exists(path))
+            {
+                return myPlugs;
+            }
+
+            string[] dlls = Directory.GetFiles(path, "*.dll");
+            foreach (var dll in dlls)
+            {
+                Assembly asm = Assembly.LoadFile(dll);
+                Type[] types = asm.GetTypes();
+                foreach (Type t in types)
+                {
+                    if (t.GetInterface("IPlugin") != null)
+                    {
+                        Plug plug = new Plug();
+                        plug.pFName = t.FullName;
+                        plug.pPath = dll;
+                        object obj = asm.CreateInstance(t.FullName);
+                        plug.pName = obj.GetType().InvokeMember("name", BindingFlags.InvokeMethod, null, obj, null).ToString();
+                        myPlugs.Add(plug);
+                    }
+                }
+            }
+            return myPlugs;
+        }
+
+        public static object CreateObject(Plug p)
+        {
+            Assembly asm = Assembly.LoadFile(p.pPath);
+            object obj = asm.CreateInstance(p.pFName);
+            return obj;
+        }
+    }
+
+}
---
+++ Plug.Reflection/WPFDesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using SDK;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFDesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Plug> myList = new List<Plug>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += Form1_Load;
+        }
+     
+        private void Form1_Load(object sender, RoutedEventArgs e)
+        {
+            myList = Kit.GetAllPlugins(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "//plugins");
+            foreach (Plug p in myList)
+            {
+
+                Button btnSubfolder = new Button();
+                btnSubfolder.Content = p.pName;
+                btnSubfolder.Margin = new Thickness(15, 15, 10, 0);
+                btnSubfolder.Width = 200;
+                btnSubfolder.Height = 150;
+                btnSubfolder.Click += b_Click;
+                btnSubfolder.HorizontalAlignment = HorizontalAlignment.Left;
+                sp.Children.Add(btnSubfolder);
+            }
+        }
+        private void b_Click(object sender, EventArgs e)
+        {
+            foreach (Plug p in myList)
+            {
+                if (p.pName == (sender as Button).Content.ToString())
+                {
+                    Run(p);
+                }
+            }
+        }
+
+        private void Run(Plug p)
+        {
+            IPlugin obj = (IPlugin)Kit.CreateObject(p);
+            textBox1.Text = obj.action(textBox1.Text);
+        }
+
+    }
+}
